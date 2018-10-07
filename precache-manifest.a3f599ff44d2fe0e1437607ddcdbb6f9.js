986e01c7df0ddeebc3e4e65caaa179a4434b3431
Jobe Range <rangejobe@gmail.com>
2018-10-06 23:51:17 -0700

    Updates

---
+++ precache-manifest.a3f599ff44d2fe0e1437607ddcdbb6f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "85ee596371c44e77bdb0",
+    "url": "/clicky-game/static/js/main.85ee5963.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/clicky-game/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "85ee596371c44e77bdb0",
+    "url": "/clicky-game/static/css/main.04e0073f.chunk.css"
+  },
+  {
+    "revision": "6bb5117a6f853f7ee8089367a944093b",
+    "url": "/clicky-game/index.html"
+  }
+];
